7407f05a4ebf34edaef97a8d850e56462ec23138
ericp <eric.proud11@gmail.com>
2025-08-26 12:33:16 -0400

    Add: Make Playlist component

---
+++ src/assets/components/Playlist.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TrackList from './TrackList';
+
+function Playlist({ playlistTracks, onRemove, onNameChange, onSave }) {
+
+    const handleChange = (e) => {
+        onNameChange(e.target.value);
+    }
+
+    return (
+        <div className="Playlist">
+            <input className="PlaylistName" onChange={handleChange} defaultValue={"New Playlist"} />
+            <TrackList tracks={playlistTracks} onRemove={onRemove} />
+            <button onClick={onSave}>
+                Save Playlist
+            </button>
+        </div>
+    )
+}
