7fd3635885796785dae634eee1618b456ad11d42
ericp <eric.proud11@gmail.com>
2025-08-27 14:25:30 -0400

    FIx: Chang URI encoding

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -5,7 +5,7 @@ function RequestAuthorizationButton() {
     const redirectUri = import.meta.env.VITE_REDIRECT_URI;
     const baseUrl = 'https://accounts.spotify.com/authorize';
     const scopes = ['playlist-modify-private', 'playlist-modify-public'];
-    const authenticationUrl = `${baseUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=code&show_dialog=true`;
+    const authenticationUrl = `${baseUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=code&show_dialog=true`;
 
     return (
         <a className="btn btn-primary" href={authenticationUrl}>
