faf24e655c276d3280663f9d3d4bb183eb610d57
ericp <eric.proud11@gmail.com>
2025-08-30 11:48:23 -0400

    Change: Change authorization flow

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
-function RequestAuthorizationButton() {
-    const clientId = import.meta.env.VITE_CLIENT_ID;
-    const redirectUri = import.meta.env.VITE_REDIRECT_URI;
-    const baseUrl = 'https://accounts.spotify.com/authorize';
-    const scopes = ['playlist-modify-private', 'playlist-modify-public'];
-    const authenticationUrl = `${baseUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=code&show_dialog=true`;
+let accessToken = '';
 
+const tempTest = () => {
     return (
-        <a className="btn btn-primary" href={authenticationUrl}>
-            Authorize Spotify
-        </a>
-    );
+        <>
+            <button onClick={SpotifyAPI.getAccessToken}>
+                Authorize Spotify
+            </button>
+            <button onClick={console.log(accessToken)}>
+                Test
+            </button>
+        </>
+    )
 }
 
-export default RequestAuthorizationButton;
+const SpotifyAPI = {
+    getAccessToken() {
+        // Case 1: accessToken is already set
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // Case 2: accessToken is in the URL
+        const FoundAccessToken = window.location.href.match(/access_token=([^&]*)/);
+        const FoundexpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+        if (FoundAccessToken && FoundexpiresIn) {
+            accessToken = FoundAccessToken[1];
+            const expiresIn = Number(FoundexpiresIn[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState({token: accessToken}, null, '/'); // Clear the URL parameters
+            return accessToken;
+        }
+        // Case 3: No accessToken, redirect to Spotify authorization
+        else {
+
+        }
+        const clientId = import.meta.env.VITE_CLIENT_ID;
+        const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+        const scopes = ['playlist-modify-private', 'playlist-modify-public'];
+        const baseUrl = 'https://accounts.spotify.com/authorize';
+        const authenticationUrl = `${baseUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=token&show_dialog=true`;
+
+        // Redirect to Spotify authorization URL
+        window.location.href = authenticationUrl;
+    }
+}
