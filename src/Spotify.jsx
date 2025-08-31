efe56695a3c786b0e471798e36d6654c9848206a
ericp <eric.proud11@gmail.com>
2025-08-31 12:50:42 -0400

    new

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -79,26 +79,25 @@ const getToken = async (code) => {
   localStorage.setItem('access_token', data.access_token);
 };
 
-const searchSpotify = (term) => {
-  const accessToken = Spotify.getAccessToken();
-  return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+export const searchSpotify = async (term) => {
+  const accessToken = localStorage.getItem('access_token');
+  if (!accessToken) throw new Error('No access token available');
+  const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${encodeURIComponent(term)}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`
     }
-  }).then(response => {
-    return response.json();
-  }).then(jsonResponse => {
-    if (!jsonResponse.tracks) {
-      return [];
-    }
-    return jsonResponse.tracks.items.map(track => ({
-      id: track.id,
-      name: track.name,
-      artist: track.artists[0].name,
-      album: track.album.name,
-      uri: track.uri
-    }));
   });
+  const jsonResponse = await response.json();
+  if (!jsonResponse.tracks) {
+    return [];
+  }
+  return jsonResponse.tracks.items.map(track => ({
+    id: track.id,
+    name: track.name,
+    artist: track.artists[0].name,
+    album: track.album.name,
+    uri: track.uri
+  }));
 };
 
 export { redirectToAuth, getCodeFromRedirectUri, getToken, searchSpotify };
