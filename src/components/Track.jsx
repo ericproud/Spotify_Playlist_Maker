398cb786060018d3b227342409fc69fb59233e5c
ericp <eric.proud11@gmail.com>
2025-09-06 12:53:20 -0400

    g

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -6,7 +6,7 @@ function Track({ track, onAdd, onRemove, addMode }) {
 
     return (
       <Card bg="dark" text="light" className="px-1 py-1" >
-        <Card.Body className="py-1 px-1">
+        <Card.Body className="py-1 px-1 flex-grow-1 me-2">
           <div className="d-flex justify-content-between align-items-center">
             <div className="text-start">
               <Card.Title className="ps-2">{track.name}</Card.Title>
