6baaac627b32b87c19c3f6a6cd9071c33a7241e5
ericp <eric.proud11@gmail.com>
2025-09-07 10:04:53 -0400

    fix

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -4,52 +4,26 @@ import Button from 'react-bootstrap/Button';
 
 function Track({ track, onAdd, onRemove, addMode }) {
 
-    return (
-      <Card bg="dark" text="light" className="px-1 py-1" >
-        <Card.Body>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex flex-column flex-grow-1 me-2">
-              <Card.Title>{track.name}</Card.Title>
-              <Card.Text className="text-muted">
-                {track.artists ? track.artists.join(", ") : ""} | {track.album}
-              </Card.Text>
-            </div>
-            <Button className="text-end"
-              variant={addMode ? "success" : "danger"}
-              onClick={() => addMode ? onAdd(track) : onRemove(track)}
-            >
-              {addMode ? "+" : "-"}
-            </Button>
+  return (
+    <Card bg="dark" text="light" className="px-1 py-1" >
+      <Card.Body>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="d-flex flex-column flex-grow-1 me-2 text-start">
+            <Card.Title>{track.name}</Card.Title>
+            <Card.Text className="text-white-50">
+              {track.artists ? track.artists.join(", ") : ""} | {track.album}
+            </Card.Text>
           </div>
-        </Card.Body>
-      </Card>
-
-      /*
-        <div>
-            <h2 className="songTitle">
-                {track.name}
-            </h2>
-            <h3 className="songInfo">
-                {track.artists ? track.artists.join(', ') : ''} | {track.album}
-            </h3>
-            {
-                addMode ?
-                    (
-                        <button onClick={() => onAdd(track)} >
-                            +
-                        </button>
-                    )
-                    :
-                    (
-                        <button onClick={() => onRemove(track)} >
-                            -
-                        </button>
-                    )
-            }
+          <Button 
+            variant={addMode ? "success" : "danger"}
+            onClick={() => addMode ? onAdd(track) : onRemove(track)}
+          >
+            {addMode ? "+" : "-"}
+          </Button>
         </div>
-
-      */
-    )
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default Track;
