dd5c02961f25d2e2a368490e7e4e020d682a4430
ericp <eric.proud11@gmail.com>
2025-09-07 12:29:15 -0400

    c

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { redirectToAuth } from '../util/Spotify';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';  
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Authorization() {
   const handleLogin = () => {
