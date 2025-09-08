a7d063c197435332bbd5a9cbfe12a5af90745bf9
ericp <eric.proud11@gmail.com>
2025-09-08 10:01:53 -0400

    o

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -13,15 +13,21 @@ function Authorization() {
   };
 
   return (
-    <Card className="w-100 h-100">
-      <Card.Body>
-        <Card.Title>Welcome to Spotify Playlist Maker</Card.Title>
-        <Card.Text>Please login with your Spotify account to continue</Card.Text>
-      </Card.Body>
-      <Button variant="success" size="lg" onClick={handleLogin}>
-        Login
-      </Button>
-    </Card>
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Row>
+        <Col className="col-12">
+          <Card className="w-100 h-100">
+            <Card.Body>
+              <Card.Title>Welcome to Spotify Playlist Maker</Card.Title>
+              <Card.Text>Please login with your Spotify account to continue</Card.Text>
+            </Card.Body>
+            <Button variant="success" size="lg" onClick={handleLogin}>
+              Login
+            </Button>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
